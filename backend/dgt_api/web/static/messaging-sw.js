cb967c390f4a0302c5975b1466e5ded9224ca8fa
Juan Jose Dominguez Barrero <jjdominguezbarrero@gmail.com>
2026-08-09 12:31:32 +0200

    Fuerza la renovacion de estilos de la PWA

--- backend/dgt_api/web/static/messaging-sw.js
+++ backend/dgt_api/web/static/messaging-sw.js
@@ -1,5 +1,5 @@
-const CACHE='gestinem-messaging-v3';
-const ASSETS=['/mensajes','/static/messaging.css','/static/messaging.js','/static/messaging.webmanifest','/static/gestinem-logo.png'];
+const CACHE='gestinem-messaging-v4';
+const ASSETS=['/mensajes','/static/messaging.css?v=3','/static/messaging.js','/static/messaging.webmanifest','/static/gestinem-logo.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
 self.addEventListener('fetch',event=>{if(event.request.method==='GET'&&new URL(event.request.url).origin===location.origin)event.respondWith(fetch(event.request).catch(()=>caches.match(event.request))) });
