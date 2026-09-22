9de3a94719b5f311f80ee0e3ed2ba2d11af39b68
Juan Jose Dominguez Barrero <jjdominguezbarrero@gmail.com>
2026-09-22 13:24:15 +0200

    Preparar versiÃ³n 1.8.19

--- app_version.py
+++ app_version.py
@@ -1,5 +1,5 @@
-APP_VERSION = "1.8.18"
-APP_RELEASE_DATE = "2026-09-20"
+APP_VERSION = "1.8.19"
+APP_RELEASE_DATE = "2026-09-22"
 
 # URL publica donde se aloja el archivo version.json con la info de actualizaciones.
 # Se publica en GitHub Raw a partir del archivo updates/version.json del repositorio.
